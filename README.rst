55dee0cc29bd112a9acc0c863f11aa837b2ec51f
adam <adam.serafini@dmc.amcnetworks.com>
2015-01-16 10:35:45 +0100

    Update README.

--- README.rst
+++ README.rst
@@ -3,7 +3,32 @@ FreeIPA-BackDoor
 ================
 
 DO NOT USE IN PRODUCTION
-========================
+------------------------
 
-An absolutely horrendous (and temporary) back door that must be installed on the test
-FreeIPA server to facilitate API access.
+An absolutely horrendous (and temporary) back door that can be installed on a
+FreeIPA server to facilitate API access by external clients, without mucking
+around with Kerberos certificates for the external client.
+A typical use case might be that you are prototyping against the excellent,
+but poorly documented FreeIPA JSON API.
+
+Instructions
+------------
+
+1. Install the dependencies:
+
+    $ pip install -r requirements.txt
+
+2. Environment variables must be set on the target box, eg:
+
+    $ export FREEIPA_URL=https://your.freeipa.net/
+
+    $ export KRB_PRINCIPAL=admin
+
+    $ export KRB_PRINCIPAL=password
+
+3. Run the software:
+
+    $ python backdoor.py
+
+Congratulations. Your external client can now access the FreeIPA JSON API
+on port 8081.
--- backdoor.py
+++ backdoor.py
@@ -3,17 +3,23 @@ import sys
 from bottle import run, request, post
 import json
 
-def call_freeipa(json_command):
-  curl = 'curl -s -H referer:https://ds02.cs.dmc-int.net/ipa ' + \
-         '-H "Content-Type:application/json" ' + \
-         '-H "Accept:application/json" --negotiate -u : --cacert /etc/ipa/ca.crt ' + \
-         '-d ' +  "'" + json_command + "'" \
-         ' -X POST https://ds02.cs.dmc-int.net/ipa/json'
+from os import environ
 
-  return subprocess.check_output(curl, shell=True)
+freeipa_url = environ['FREEIPA_URL']
+
+
+def call_freeipa(json_command):
+    curl = 'curl -s -H referer:%sipa ' % freeipa_url + \
+        '-H "Content-Type:application/json" ' + \
+        '-H "Accept:application/json" --negotiate -u : --cacert /etc/ipa/ca.crt ' + \
+        '-d ' +  "'" + json_command + "'" \
+        ' -X POST %sipa/json' % freeipa_url
+
+    return subprocess.check_output(curl, shell=True)
+
 
 @post('/api')
 def api():
-  return json.loads(call_freeipa(json.dumps(request.json)))
+    return json.loads(call_freeipa(json.dumps(request.json)))
 
 run(host='0.0.0.0', port=8081)
