48bd30d682ff6c2d3a39bd767396cce0fb392a73
root <root@sandbox04.cs-dev.dmc-int.net>
2015-01-16 15:41:22 +0100

    Call kinit with credentials before accessing API.

--- backdoor.py
+++ backdoor.py
@@ -6,16 +6,20 @@ import json
 from os import environ
 
 freeipa_url = environ['FREEIPA_URL']
+krb_principal = environ['KRB_PRINCIPAL']
+krb_password = environ['KRB_PASSWORD']
 
+def call_freeipa(json_req):
 
-def call_freeipa(json_command):
+    kinit = 'echo %s | kinit %s >/dev/null' % (krb_password, krb_principal)
+
     curl = 'curl -s -H referer:%sipa ' % freeipa_url + \
         '-H "Content-Type:application/json" ' + \
         '-H "Accept:application/json" --negotiate -u : --cacert /etc/ipa/ca.crt ' + \
-        '-d ' +  "'" + json_command + "'" \
+        '-d ' +  "'" + json_req + "'" \
         ' -X POST %sipa/json' % freeipa_url
 
-    return subprocess.check_output(curl, shell=True)
+    return subprocess.check_output('%s; %s' % (kinit, curl), shell=True)
 
 
 @post('/api')
